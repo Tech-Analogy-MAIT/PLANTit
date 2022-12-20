f57802960cb12ee79eac81790626bb21ce8b2525
Chanmeet <chanmeetsinghsahni@gmail.com>
2022-12-20 23:24:28 +0530

    Commit on 20th Dec 11:30 pm

--- PlantItFinal/home/views.py
+++ PlantItFinal/home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse
+from home.models import Contact
 
 # Create your views here.
 def index(request) :
@@ -8,6 +9,13 @@ def home(request) :
 def about(request) :
     return render(request, 'about.html')
 def contact(request) :
+    if request.method == "POST" :
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        contact = Contact(name=name, email=email, phone=phone, message=message)
+        contact.save()
     return render(request, 'contact.html')
 def social(request) :
     return render(request, 'social.html')
